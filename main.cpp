989b3bc07739d954ee177a768d49346256d2715f
amark2005 <amarkeshav14@gmail.com>
2026-02-18 22:13:21 +0530

    collision added

--- main.cpp
+++ main.cpp
@@ -4,16 +4,21 @@ int main(){
   SetTargetFPS(24);
   int ballx=400;
   int bally=400;
-  
+  int ballspeed=10;
   //game loop
   while(WindowShouldClose() == false){
     //1. event handling here
-    if(IsKeyDown(KEY_W)){bally-=5;}
-    if(IsKeyDown(KEY_S)){bally+=5;}
-    if(IsKeyDown(KEY_A)){ballx-=5;}
-    if(IsKeyDown(KEY_D)){ballx+=5;}
+    if(IsKeyDown(KEY_W)){bally-=ballspeed;}
+    if(IsKeyDown(KEY_S)){bally+=ballspeed;}
+    if(IsKeyDown(KEY_A)){ballx-=ballspeed;}
+    if(IsKeyDown(KEY_D)){ballx+=ballspeed;}
 
     //2. position update here
+    if(ballx >= GetScreenWidth()){ballx=GetScreenWidth();}
+    if(ballx <= 0){ballx=0;}
+    if(bally >= GetScreenHeight()){bally=GetScreenHeight();}
+    if(bally <= 0){bally=0;}
+    
 
 
     //3. Drawing here
